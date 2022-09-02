abe5835e568f6293cc8336a46e8dd3266dd58d8f
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-09-03 04:02:55 +0600

    author name and view added

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const displayCategory = names => {
     names.forEach(name => {
         const nameDiv = document.createElement('div');
         nameDiv.innerHTML =` 
-        <button onclick="loadCategoryDetailes('${name.category_id}')" class="btn btn-light text-secondary" data-bs-toggle="modal" data-bs-target="#categoryModal">${name.category_name}</button>
+        <button onclick="loadCategoryDetailes('${name.category_id}')" class="btn btn-light text-secondary">${name.category_name}</button>
         `;
         nameContainer.appendChild(nameDiv);
     });
@@ -19,11 +19,11 @@ const displayCategory = names => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url);
     const data = await res.json();
-    displayCategoryDetailes(data.data);
+    displayCategoryDetails(data.data);
   }
 
-  const displayCategoryDetailes = categories => {
-    console.log(categories);
+  const displayCategoryDetails = categories => {
+    // console.log(categories);
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
         const categoryDiv = document.createElement('div');
@@ -35,22 +35,22 @@ const displayCategory = names => {
                   <div class="col-md-8">
                     <div class="card-body">
                       <h5 class="card-title">${category.title}</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p class="card-text">${category.details}</p>
                       
                     </div>
                     <div class="card-footer border border-0">
                         <div class="d-flex justify-content-evenly p-3">
                             <div class="d-flex">
                                 <div>
-                                    <img src="" class="img-fluid rounded" alt="...">
+                                    <img src="" class="img-thumbnail rounded" alt="...">
                                 </div>
                                 <div>
-                                    <h5>Jane Cooper</h5>
-                                <small>jan 10, 2022</small>
+                                    <h5>${category.author.name}</h5>
+                                <small>${category.author.published_date}</small>
                                 </div>
                             </div>
                             <div>
-                                <i class="fa-regular fa-eye fw-bold"> 1.5 M</i>
+                                <i class="fa-regular fa-eye fw-bold"> ${category.total_view}</i>
                             </div>
                             <div>
                                 <button class="btn btn-primary">Show Details</button>
@@ -59,7 +59,7 @@ const displayCategory = names => {
                       </div>
                   </div>
         `;
-        categoryContainer.appendChild(categoryDiv);c
+        categoryContainer.appendChild(categoryDiv);
     })
 
   }
