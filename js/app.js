be53658f5ecef8ede3e1982d44179633eef7731b
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-09-03 00:44:18 +0600

    category section added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('hehehehe');
+const loadCategory = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategory(data.data.news_category)
+}
+
+const displayCategory = names => {
+    const nameContainer = document.getElementById('name-container');
+    names.forEach(name => {
+        const nameDiv = document.createElement('div');
+        nameDiv.innerHTML =` 
+        <button id="category_id-$01" class="btn btn-light text-secondary">${name.category_name}</button>
+        `;
+        nameContainer.appendChild(nameDiv);
+    });
+}
+
+loadCategory();
